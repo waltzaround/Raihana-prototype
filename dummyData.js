572a1f97886286f944597baaf5d6f6a1796147f9
Bai Wang <yekbaiwang93@gmail.com>
2019-03-10 11:44:48 +1300

    implement scan functionality to render data based on Qr code dynamically

---
+++ dummyData.js
@@ -0,0 +1,26 @@
+var dummyData = [{
+  licenseId: 'DS59-546-2219',
+  first_name: 'Micki',
+  last_name: 'Tuffrey',
+  email: 'mtuffrey0@jugem.jp',
+  gender: 'Male',
+  age: 28,
+  issued: '15/12/2012',
+  expiry: '8/11/1992',
+  dateOfBirth: '8/11/1992',
+  address: '6038 Bay Center',
+  profile_image: 'https://images.unsplash.com/photo-1552056613-1f3710d175ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=989&q=80'
+},
+{
+  licenseId: 'DS74-063-0160',
+  first_name: 'Austin',
+  last_name: 'Petken',
+  email: 'apetken1@eepurl.com',
+  gender: 'Female',
+  age: 16,
+  issued: '15/12/2012',
+  expiry: '8/11/1992',
+  dateOfBirth: '8/11/2003',
+  address: '6038 Bay Center',
+  profile_image: 'https://images.unsplash.com/photo-1552046125-a89ffeb451b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+}]
